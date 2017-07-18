5abea367936df90f1a1d413bffc6e27e090b8104
Jenmadev <info@maknowsthat.com>
2017-07-17 23:29:05 -0700

    Timer Running + Hide Questions & Answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,43 @@ var questions;
 var currentQuestion = 0;
 var clockRunning = false;
 
-console.log("dave");
+//  Variable that will hold our setInterval that runs the stopwatch
+var intervalId;
 
+//prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+
+//  Our stopwatch object.
+var stopwatch = {
+  time: 10,
+  reset: function() {
+    stopwatch.time = 0;  
+  },
+  start: function() {
+      console.log("start");
+      if (!clockRunning) {
+        intervalId = setInterval(stopwatch.count, 1000);
+        clockRunning = true;
+      }
+  },
+  stop: function() {
+    clearInterval(intervalId);
+    clockRunning = false;
+  },
+  count: function() {
+    stopwatch.time--;
+     $("#timecounter").html(stopwatch.time);
+    if (stopwatch.time === 0){
+     stopwatch.stop();
+     resultMessage("test","timeout");
+
+    }
+  },
+}
+
+
+console.log("dave");
+//Start Game
 $("#startButton").click(getQuestions);
 
 function startGame() {
@@ -14,7 +49,7 @@ function startGame() {
 	displayQuestion();
 }
 
-
+//Get Question from API
 function getQuestions(){
 	$.ajax({
 	url:queryURL,
@@ -25,7 +60,7 @@ function getQuestions(){
 		startGame();
 	});
 }
-
+//End of Game
 function gameOver(){
 	$("#startButton").show();	
 }
@@ -36,11 +71,24 @@ function displayQuestion(){
 	for(var i = 0; i < question.incorrect_answers.length; i++){
 		choices += "<br>" + question.incorrect_answers[i];
 	}
-	console.log(question +1 + ": " + question.question);
-	$("#question").html(question.question);
+	$("#display").html("Time Remaining: "+ '<span id="timecounter">30</span>');
+	$("#question").html("Question " + +1 + ": " + question.question);
 	$("#choices").html(choices);
-	currentQuestion++;
+	stopwatch.start();
 
-	//TO DO: Make an empty div
-	//	
+};
+
+
+function resultMessage(correctanswer,scenario){
+	$("#question").hide();
+	$("#choices").hide();
+	if(scenario ==="correctanswer"){
+
+	}
+	else if(scenario === "wronganswer"){
+
+	}
+	else {
+		console.log("BOOB");
+	}
 }
