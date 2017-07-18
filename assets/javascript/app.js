0382247af7b71ac0cb6b1118afb35b9609ca09ab
Jenmadev <info@maknowsthat.com>
2017-07-18 13:36:06 -0700

    added choice clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,20 +2,21 @@
 //Global Variables
 var queryURL = "https://opentdb.com/api.php?amount=10&category=21&type=multiple";
 var questions;
+var question;
 var currentQuestion = 0;
 var clockRunning = false;
-
-//  Variable that will hold our setInterval that runs the stopwatch
 var intervalId;
+var correctQuestions;
+var incorrectQuestions;
+var unansweredQuestions;
 
-//prevents the clock from being sped up unnecessarily
-var clockRunning = false;
+
 
 //  Our stopwatch object.
 var stopwatch = {
   time: 10,
   reset: function() {
-    stopwatch.time = 0;  
+    stopwatch.time = 10;  
   },
   start: function() {
       console.log("start");
@@ -33,7 +34,7 @@ var stopwatch = {
      $("#timecounter").html(stopwatch.time);
     if (stopwatch.time === 0){
      stopwatch.stop();
-     resultMessage("test","timeout");
+     resultMessage(question.correct_answer,"timeout");
 
     }
   },
@@ -46,6 +47,10 @@ $("#startButton").click(getQuestions);
 
 function startGame() {
 	$("#startButton").hide();
+	correctQuestions = 0;
+	incorrectQuestions = 0;
+	unansweredQuestions = 0;
+	currentQuestion = 0;
 	displayQuestion();
 }
 
@@ -62,33 +67,64 @@ function getQuestions(){
 }
 //End of Game
 function gameOver(){
-	$("#startButton").show();	
+	$("#startButton").show();
+	$("#result").empty();
+	$("#result").html("Here are your results: </br> Correct Answers: " + correctQuestions + "</br> Wrong Answers: " + incorrectQuestions + "</br> No Answers: " + unansweredQuestions);
 }
 
 function displayQuestion(){
-	var question = questions[currentQuestion];
-	var choices = question.correct_answer;
+	$("#question").show();
+	$("#choices").show();
+	$("#result").hide();
+	$("#display").show();
+	stopwatch.reset();
+	question = questions[currentQuestion];
+	
+	var choices = '<span class ="choice">' + question.correct_answer +'</span>';
 	for(var i = 0; i < question.incorrect_answers.length; i++){
-		choices += "<br>" + question.incorrect_answers[i];
+		choices += ('<span class ="choice">' + question.incorrect_answers[i] +'</span>');
 	}
-	$("#display").html("Time Remaining: "+ '<span id="timecounter">30</span>');
-	$("#question").html("Question " + +1 + ": " + question.question);
+	$("#display").html("Time Remaining: "+ '<span id="timecounter">10</span>');
+	$("#question").html("Question " + (currentQuestion +1) + ": " + question.question);
 	$("#choices").html(choices);
+	$(".choice").click(function(){
+		stopwatch.stop();
+		var choice = $(this);
+		var selectedAnswer = choice.text();
+		if(selectedAnswer ===question.correct_answer){
+			resultMessage(selectedAnswer,"correctanswer");
+		}
+		else{
+			resultMessage(selectedAnswer,"wronganswer");
+		}
+	});
 	stopwatch.start();
-
+	currentQuestion++;
 };
 
 
 function resultMessage(correctanswer,scenario){
 	$("#question").hide();
 	$("#choices").hide();
+	$("#result").show();
+	$("#display").hide();
 	if(scenario ==="correctanswer"){
-
+		$("#result").html("YESSSS! " + "</br>Correct Answer: " + question.correct_answer);
+		correctQuestions++;
 	}
 	else if(scenario === "wronganswer"){
-
+		$("#result").html("NOPEEEEE! " + "</br>Correct Answer: " + question.correct_answer);
+		incorrectQuestions++;
 	}
 	else {
-		console.log("BOOB");
+		$("#result").html("OUT OF TIME! " + "</br>Correct Answer: " + question.correct_answer);
+		unansweredQuestions++;
+		
+	}
+	if (currentQuestion === 10){
+		gameOver();
+	}
+	else{
+		setTimeout(displayQuestion, 3000);
 	}
 }
